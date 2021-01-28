f2e071a20363a5089c55ec3d2fcf6a62b2cad448
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2021-01-28 11:37:46 +0530

    find binlogfile version

--- file.go
+++ file.go
@@ -18,15 +18,15 @@ type binlogFile struct {
 	binlogPos    uint32
 }
 
-// todo: https://dev.mysql.com/doc/internals/en/determining-binary-log-version.html
-
 func Open(file string) (*binlogFile, error) {
 	f, err := openBinlogFile(file)
 	if err != nil {
 		return nil, err
 	}
 	return &binlogFile{
-		conn: f,
+		conn:       f,
+		binlogFile: file,
+		binlogPos:  4,
 	}, nil
 }
 
@@ -46,14 +46,49 @@ func openBinlogFile(file string) (*os.File, error) {
 	return f, nil
 }
 
+// todo: https://dev.mysql.com/doc/internals/en/determining-binary-log-version.html
+func findBinlogVersion(file string) (uint16, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+	r := &reader{rd: f, limit: -1}
+	r.skip(4) // magic number
+	r.skip(4)
+	eventType := r.int1()
+	r.skip(4)
+	eventSize := r.int4()
+	if r.err != nil {
+		return 0, r.err
+	}
+	if eventType != START_EVENT_V3 && eventType != FORMAT_DESCRIPTION_EVENT {
+		return 3, nil
+	}
+	if eventType == START_EVENT_V3 {
+		if eventSize < 75 {
+			return 1, nil
+		}
+		return 3, nil
+	}
+	if eventType == FORMAT_DESCRIPTION_EVENT {
+		return 4, nil
+	}
+	return 0, fmt.Errorf("binlog.findBinlogVersion: cannot determine for %q", file)
+}
+
 func (c *binlogFile) nextEvent() (interface{}, error) {
 	r := c.binlogReader
 	if r == nil {
+		v, err := findBinlogVersion(c.binlogFile)
+		if err != nil {
+			return nil, err
+		}
 		r = &reader{
 			rd:    c.conn,
 			limit: -1,
 		}
-		r.fde = formatDescriptionEvent{binlogVersion: 4}
+		r.fde = formatDescriptionEvent{binlogVersion: v}
 		c.binlogReader = r
 	} else {
 		fmt.Println("draining")
