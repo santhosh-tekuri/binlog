0865a2faffb6a6d03d095eb145df431e4c5fe20e
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2021-01-30 16:16:23 +0530

    cleanup files

--- conn.go
+++ conn.go
@@ -166,3 +166,26 @@ func (c *conn) nextEvent() (interface{}, error) {
 func (c *conn) Close() error {
 	return c.conn.Close()
 }
+
+// comBinlogDump ---
+
+const (
+	COM_BINLOG_DUMP       = 0x12
+	BINLOG_DUMP_NON_BLOCK = 0x01
+)
+
+type comBinlogDump struct {
+	binlogPos      uint32
+	flags          uint16
+	serverID       uint32
+	binlogFilename string
+}
+
+func (e comBinlogDump) writeTo(w *writer) error {
+	w.int1(COM_BINLOG_DUMP)
+	w.int4(e.binlogPos)
+	w.int2(e.flags)
+	w.int4(e.serverID)
+	w.string(e.binlogFilename)
+	return w.err
+}
--- generic.go
+++ generic.go
@@ -2,6 +2,34 @@ package binlog
 
 import "fmt"
 
+// eofPacket ---
+
+// https://dev.mysql.com/doc/internals/en/packet-EOF_Packet.html
+
+const eofMarker = 0xfe
+
+type eofPacket struct {
+	warnings    uint16
+	statusFlags uint16
+}
+
+func (e *eofPacket) parse(r *reader) error {
+	header := r.int1()
+	if r.err != nil {
+		return r.err
+	}
+	if header != eofMarker {
+		return fmt.Errorf("eofPacket.parse: got header %0xd", header)
+	}
+	e.warnings = r.int2()
+	e.statusFlags = r.int2()
+	return r.err
+}
+
+// errPacket ---
+
+// https://dev.mysql.com/doc/internals/en/packet-ERR_Packet.html
+
 const (
 	errMarker = 0xFF
 	okMarker  = 0x00
